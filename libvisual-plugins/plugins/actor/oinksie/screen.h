af00a503a82aff94ecc7cf8779828e7806e3cdcf
no author <noone@nowhere>
2004-09-10 17:03:02 +0000

    This commit was manufactured by cvs2svn to create tag 'LIBVISUAL_0_1_6'.

--- libvisual-plugins/plugins/actor/oinksie/screen.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OINK_SCREEN_H
-#define _OINK_SCREEN_H
-
-#include "oinksie.h"
-
-void _oink_screen_init (OinksiePrivate *priv, int width, int height);
-void _oink_screen_size_set (OinksiePrivate *priv, int width, int height);
-
-#endif /* _OINK_SCREEN_H */
