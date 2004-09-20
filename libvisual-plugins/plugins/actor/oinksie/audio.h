94f90f2385892d3c315ce703d075d9a6399ff4ce
no author <noone@nowhere>
2004-09-20 18:33:30 +0000

    This commit was manufactured by cvs2svn to create tag 'LIBVISUAL_GFORCE_0_1_0'.

--- libvisual-plugins/plugins/actor/oinksie/audio.h
+++
@@ -1,9 +0,0 @@
-#ifndef _OINK_AUDIO_H
-#define _OINK_AUDIO_H
-
-#include "oinksie.h"
-
-void _oink_audio_init (OinksiePrivate *priv);
-void _oink_audio_analyse (OinksiePrivate *priv);
-
-#endif /* _OINK_AUDIO_H */
