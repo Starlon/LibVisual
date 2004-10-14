0315b3cb79f08732504ceaf0c7c3e44f1e7b270b
no author <noone@nowhere>
2004-10-14 13:36:07 +0000

    This commit was manufactured by cvs2svn to create tag 'LIBVISUAL_GFORCE_0_1_1'.

--- libvisual-plugins/plugins/actor/JESS/distorsion.h
+++
@@ -1,8 +0,0 @@
-#include "jess.h"
-
-void create_tables(JessPrivate *priv);
-void rot_hyperbolic_radial(float *n_fx,float *n_fy,float d_alpha, float rad_factor, float cx, float cy);
-void rot_cos_radial( float *n_fx,float *n_fy,float d_alpha, float rad_factor, float cx, float cy);
-void homothetie_hyperbolic(float *n_fx,float *n_fy, float rad_factor, float cx, float cy);
-void homothetie_cos_radial(float *n_fx,float *n_fy, float rad_factor, float cx, float cy);
-void noize(JessPrivate *priv, float *n_fx,float *n_fy, float intensity);
