bc68bd667bb097729b5e36720600fd61e8151a03
Scott Sibley <ssibley@gmail.com>
2010-06-04 01:26:14 +0000

    Add all spidermonkey source

--- actor_superscope.c
+++ actor_superscope.c
@@ -32,6 +32,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include "jsapi.h"
 
 #include <libvisual/libvisual.h>
 
@@ -46,13 +47,37 @@ enum scope_runnable {
     SCOPE_RUNNABLE_POINT,
 };
 
+JSBool prop_getter(JSContext *cx, JSObject *obj, jsval idval, jsval *vp);
+JSBool prop_setter(JSContext *cx, JSObject *obj, jsval idval, jsval *vp);
+
+enum my_tinyid {
+    MY_N, MY_B, MY_X, MY_Y, MY_I, MY_V, MY_W, MY_H, MY_RED, MY_GREEN, MY_BLUE, MY_LINESIZE, MY_SKIP, MY_DRAWMODE
+};
+
+static JSPropertySpec my_props[] = {
+    {"n", MY_N, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"b", MY_B, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"x", MY_X, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"y", MY_Y, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"i", MY_I, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"v", MY_V, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"w", MY_W, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"h", MY_H, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"red", MY_RED, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"green", MY_GREEN, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"blue", MY_BLUE, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"linesize", MY_LINESIZE, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"skip", MY_SKIP, JSPROP_ENUMERATE, prop_getter, prop_setter},
+    {"drawmode", MY_DRAWMODE, JSPROP_ENUMERATE, prop_getter, prop_setter},
+};
+
 typedef struct {
     /*AvsRunnableContext      *ctx;
     AvsRunnableVariableManager  *vm;
     AvsRunnable         *runnable[4];
-    AvsNumber           n, b, x, y, i, v, w, h, red, green, blue, linesize, skip, drawmode; 
     AvsGlobalProxy      *proxy;
     */
+    double n, b, x, y, i, v, w, h, red, green, blue, linesize, skip, drawmode; 
 
     char            *point;
     char            *frame;
@@ -67,8 +92,10 @@ typedef struct {
 
     VisAudio    *audio;
 
-    AVSGfxColorCycler   *cycler;
+    //AVSGfxColorCycler   *cycler;
 } SuperScopePrivate;
+
+SuperScopePrivate *priv;
 
 int lv_superscope_init (VisPluginData *plugin);
 int lv_superscope_cleanup (VisPluginData *plugin);
@@ -114,6 +141,114 @@ const VisPluginInfo *get_plugin_info (int *count)
     return info;
 }
 
+JSBool prop_getter(JSContext *cx, JSObject *obj, jsval idval, jsval *vp)
+{
+    if (JSVAL_IS_INT(idval)) {
+        switch(JSVAL_TO_INT(idval)) {
+            case MY_N: 
+                *vp = priv->n;
+                break;
+            case MY_B:
+                *vp = priv->b;
+                break;
+            case MY_X:
+                *vp = priv->x;
+                break;
+            case MY_Y:
+                *vp = priv->y;
+                break;
+            case MY_I:
+                *vp = priv->i;
+                break;
+            case MY_V:
+                *vp = priv->v;
+                break;
+            case MY_W:
+                *vp = priv->w;
+                break;
+            case MY_H:
+                *vp = priv->h;
+                break;
+            case MY_RED:
+                *vp = priv->red;
+                break;
+            case MY_GREEN:
+                *vp = priv->green;
+                break;
+            case MY_BLUE:
+                *vp = priv->blue;
+                break;
+            case MY_LINESIZE:
+                *vp = priv->linesize;
+                break;
+            case MY_SKIP:
+                *vp = priv->skip;
+                break;
+            case MY_DRAWMODE:
+                *vp = priv->drawmode;
+                break;
+            default:
+                break;
+                       
+        }
+    } 
+    return JS_TRUE;
+}
+
+JSBool prop_setter(JSContext *cx, JSObject *obj, jsval idval, jsval *vp)
+{
+    if (JSVAL_IS_INT(idval)) {
+        switch(JSVAL_TO_INT(idval)) {
+            case MY_N: 
+                priv->n = *vp;
+                break;
+            case MY_B:
+                priv->b = *vp;
+                break;
+            case MY_X:
+                priv->x = *vp;
+                break;
+            case MY_Y:
+                priv->y = *vp;
+                break;
+            case MY_I:
+                priv->i = *vp;
+                break;
+            case MY_V:
+                priv->v = *vp;
+                break;
+            case MY_W:
+                priv->w = *vp;
+                break;
+            case MY_H:
+                priv->h = *vp;
+                break;
+            case MY_RED:
+                priv->red = *vp;
+                break;
+            case MY_GREEN:
+                priv->green = *vp;
+                break;
+            case MY_BLUE:
+                priv->blue = *vp;
+                break;
+            case MY_LINESIZE:
+                priv->linesize = *vp;
+                break;
+            case MY_SKIP:
+                priv->skip = *vp;
+                break;
+            case MY_DRAWMODE:
+                priv->drawmode = *vp;
+                break;
+            default:
+                break;
+                       
+        }
+    } 
+
+    return JS_TRUE;
+}
 
 int scope_load_runnable(SuperScopePrivate *priv, ScopeRunnable runnable, char *buf)
 {
@@ -136,26 +271,29 @@ int scope_run(SuperScopePrivate *priv, ScopeRunnable runnable)
 
 int lv_superscope_init (VisPluginData *plugin)
 {
-    SuperScopePrivate *priv;
     VisParamContainer *paramcontainer = visual_plugin_get_params (plugin);
     int i;
 
-    static VisParamEntryProxy params[] = {
-        VISUAL_PARAM_LIST_ENTRY_STRING ("point", "d=i+v*0.2; r=t+i*$PI*4; x = cos(r)*d; y = sin(r) * d;", ""),
-        VISUAL_PARAM_LIST_ENTRY_STRING ("frame", "t=t-0.01;", ""),
-        VISUAL_PARAM_LIST_ENTRY_STRING ("beat", "", ""),
-        VISUAL_PARAM_LIST_ENTRY_STRING ("init", "n=800;", ""),
-        VISUAL_PARAM_LIST_ENTRY_INTEGER ("channel source", 0, VISUAL_PARAM_LIMIT_NONE, ""),
+    visual_log_return_val_if_fail(priv != NULL, -VISUAL_ERROR_GENERAL);
+
+    static VisParamEntry params[] = {
+        VISUAL_PARAM_LIST_ENTRY_STRING ("point", "d=i+v*0.2; r=t+i*$PI*4; x = cos(r)*d; y = sin(r) * d;"),
+        VISUAL_PARAM_LIST_ENTRY_STRING ("frame", "t=t-0.01;"),
+        VISUAL_PARAM_LIST_ENTRY_STRING ("beat", ""),
+        VISUAL_PARAM_LIST_ENTRY_STRING ("init", "n=800;"),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("channel source", 0),
         VISUAL_PARAM_LIST_ENTRY ("palette"),
-        VISUAL_PARAM_LIST_ENTRY_INTEGER ("draw type", 0, VISUAL_PARAM_LIMIT_NONE, ""),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("draw type", 0),
         VISUAL_PARAM_LIST_END
     };
 
-    visual_param_container_add_many_proxy (paramcontainer, params);
+    visual_param_container_add_many (paramcontainer, params);
+    
 
     priv = visual_mem_new0 (SuperScopePrivate, 1);
-    priv->proxy = visual_object_get_private(VISUAL_OBJECT(plugin));
-    visual_object_ref(VISUAL_OBJECT(priv->proxy));
+
+    //priv->proxy = visual_object_get_private(VISUAL_OBJECT(plugin));
+    //visual_object_ref(VISUAL_OBJECT(priv->proxy));
     visual_object_set_private (VISUAL_OBJECT (plugin), priv);
 
     visual_palette_allocate_colors (&priv->pal, 1);
@@ -211,9 +349,9 @@ int lv_superscope_cleanup (VisPluginData *plugin)
     if(priv->init != NULL)
         visual_mem_free(priv->init);
 
-    if(priv->proxy != NULL)
+/*    if(priv->proxy != NULL)
         visual_object_unref(VISUAL_OBJECT(priv->proxy));
-
+*/
     visual_mem_free (priv);
 
     return 0;
@@ -298,12 +436,14 @@ int lv_superscope_events (VisPluginData *plugin, VisEventQueue *events)
                     visual_palette_allocate_colors (&priv->pal, pal->ncolors);
                     visual_palette_copy (&priv->pal, pal);
 
+                /*
                     if (priv->cycler != NULL)
                         visual_object_unref (VISUAL_OBJECT (priv->cycler));
 
                     priv->cycler = avs_gfx_color_cycler_new (&priv->pal);
                     avs_gfx_color_cycler_set_mode (priv->cycler, AVS_GFX_COLOR_CYCLER_TYPE_TIME);
                     avs_gfx_color_cycler_set_time (priv->cycler, avs_config_standard_color_cycler_time ());
+                */
                 }
 
                 break;
@@ -342,7 +482,7 @@ static __inline int makeint(double t)
 int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 {
     SuperScopePrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
-    AvsGlobalProxy *proxy = priv->proxy;
+    //AvsGlobalProxy *proxy = priv->proxy;
     uint32_t *buf = visual_video_get_pixels (video);
     int isBeat;
     int i;
@@ -361,7 +501,7 @@ int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audi
             1.0);
 
 
-    isBeat = proxy->isBeat;
+    isBeat = visual_audio_is_beat_with_data(audio, VISUAL_BEAT_ALGORITHM_ADV, pcmbuf, 1024);
 
     /* Provide audio for AvsRunnable */
     priv->audio = audio;
@@ -380,22 +520,6 @@ int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audi
     int xorv=(ws*128)^128;
     uint16_t fa_data[576];
 
-    if((priv->channel_source&3) >= 2)
-    {
-        for(x = 0; x < 576; x++) {
-            //fa_data[x] = proxy->audiodata[ws^1][0][x] * UINT8_MAX + proxy->audiodata[ws^1][1][x] * UINT8_MAX;
-            pcmbuf[x] = proxy->audiodata[ws^1][0][x] / 2 + proxy->audiodata[ws^1][1][x] / 2;
-        }
-    }
-    else 
-    {
-        printf(" < 2\n");
-        for(x = 0; x < 576; x++) {
-            //fa_data[x] = proxy->audiodata[ws^1][priv->channel_source&3][x] * UINT16_MAX;
-            pcmbuf[x] = proxy->audiodata[ws^1][priv->channel_source&3][x];
-        }
-    }
-    
     priv->color_pos++;
 
     if(priv->color_pos >= priv->pal.ncolors * 64) priv->color_pos = 0;
@@ -425,7 +549,7 @@ int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audi
     priv->green = ((current_color>>8)&0xff)/255.0;
     priv->red = ((current_color>>16)&0xff)/255.0;
     priv->skip = 0.0;
-    priv->linesize = (double) ((priv->proxy->line_blend_mode&0xff0000)>>16);
+    priv->linesize = 1; //(double) ((priv->proxy->line_blend_mode&0xff0000)>>16);
     priv->drawmode = priv->draw_type ? 1.0 : 0.0;
 
     scope_run(priv, SCOPE_RUNNABLE_FRAME);
@@ -446,12 +570,12 @@ int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audi
         //double yr=(fa_data[(int)r]^xorv)*(1.0f-s1)+(fa_data[(int)r+1]^xorv)*(s1);
         //priv->v = yr/128.0 - 1.0;
         priv->v = pcmbuf[a * 288 / l];
-        priv->i = (AvsNumber)a/(AvsNumber)(l-1);
+        priv->i = a/(l-1);
         priv->skip = 0.0;
         scope_run(priv, SCOPE_RUNNABLE_POINT);
 
-        x = (int)((priv->x + 1.0) * (AvsNumber)video->width * 0.5);
-        y = (int)((priv->y + 1.0) * (AvsNumber)video->height * 0.5);
+        x = (int)((priv->x + 1.0) * video->width * 0.5);
+        y = (int)((priv->y + 1.0) * video->height * 0.5);
 
         if (priv->skip >= 0.00001)
             continue;
@@ -459,8 +583,8 @@ int lv_superscope_render (VisPluginData *plugin, VisVideo *video, VisAudio *audi
         int this_color = makeint(priv->blue) | (makeint(priv->green) << 8) | (makeint(priv->red) << 16);
 
         if (priv->drawmode < 0.00001) {
-            if (y >= 0 && y < video->height && x >= 0 && x < video->width)
-                BLEND_LINE(proxy, buf+x+y*video->width, this_color);
+                //if (y >= 0 && y < video->height && x >= 0 && x < video->width)
+                //BLEND_LINE(proxy, buf+x+y*video->width, this_color);
         } else {
             if (a > 0) {
                 if (y >= 0 && y < video->height && x >= 0 && x < video->width &&
