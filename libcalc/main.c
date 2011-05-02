c8a55a2c2b32ff302d69dbd10ca283dc7c9db824
Scott Sibley <ssibley@gmail.com>
2011-05-02 02:58:08 +0000

    Clean up whitespace

--- libcalc/main.c
+++ libcalc/main.c
@@ -4,22 +4,18 @@
 
 int main(int argc, char **argv)
 {
-	double *n;
+    double *n;
 
-	symbol_dict_t *environment = dict_new();
+    symbol_dict_t *environment = dict_new();
 
+    n = dict_variable(environment, "test");
+    *n = 50;
 
-	n = dict_variable(environment, "test");
-        *n = 50;
+    expression_t *expr = expr_compile_string("test = test + 100;", environment);;
 
-	expression_t *expr = expr_compile_string("test = 100;", environment);;
+    expr_execute(expr, environment);
 
-
-
-	expr_execute(expr, environment);
-
-
-	printf("n = %f\n", *n);
-	
-	return 0;
+    printf("n = %f\n", *n);
+    
+    return 0;
 }
