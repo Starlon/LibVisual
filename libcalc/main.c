88bd63448175e90ff3d2ec860a03c87113d9b99e
Scott Sibley <ssibley@gmail.com>
2011-05-02 18:10:24 +0000

    Give libcalc a makefile

--- libcalc/main.c
+++ libcalc/main.c
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-    double *n;
+    double *n, *t;
     int i;
 
     symbol_dict_t *environment = dict_new();
@@ -12,14 +12,18 @@ int main(int argc, char **argv)
     n = dict_variable(environment, "test");
     *n = 50;
 
-    expression_t *expr = expr_compile_string("test=test+100; ", environment);;
+    expression_t *expr = expr_compile_string("test=test+100; t = t + 0.5; test=test*t;", environment);;
 
+    t = dict_variable(environment, "t");
     expr_execute(expr, environment);
+    expr_execute(expr, environment);
+    expr_execute(expr, environment);
+
 
     n = dict_variable(environment, "test");
     i = dict_lookup(environment, "test");
 
-    printf(".value = %f, *n = %f, i = %d\n", environment->variables[i].value, *n, i);
+    printf(".value = %f, *n = %f, i = %d, t = %f\n", environment->variables[i].value, *n, i, *t);
     
     return 0;
 }
