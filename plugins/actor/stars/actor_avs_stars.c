1f4551218f5405de317c31f583f0220f805ae706
Scott Sibley <ssibley@gmail.com>
2011-05-11 01:29:54 +0000

    Add stars plugin.

--- plugins/actor/stars/actor_avs_stars.c
+++ plugins/actor/stars/actor_avs_stars.c
@@ -31,11 +31,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <ststars.h>
 
 #include <libvisual/libvisual.h>
 
 #include "avs_common.h"
+#include "lvavs_pipeline.h"
 
 typedef struct {
     int X, Y;
@@ -45,7 +45,7 @@ typedef struct {
 } StarFormat;
 
 typedef struct {
-    AvsGlobalProxy *proxy;
+    LVAVSPipeline *pipeline;
     VisPalette pal;
 
     int enabled;
@@ -53,10 +53,10 @@ typedef struct {
     int Xoff;
     int Yoff;
     int Zoff;
-    float WarpSpeed;
+    float warpSpeed;
     int blend;
     int blendavg;
-    StarFormat Stars[4096]
+    StarFormat Stars[4096];
     int Width, Height;
     int onbeat;
     int spdBeat;
@@ -65,7 +65,13 @@ typedef struct {
     float CurrentSpeed;
     int nc;
 
+    AVSGfxColorCycler *cycler;
+
 } StarsPrivate;
+
+void initialize_stars(StarsPrivate *priv);
+void create_star(StarsPrivate *priv, int A);
+static unsigned int __inline BLEND_ADAPT(unsigned int a, unsigned int b, /*float*/int divisor);
 
 int lv_stars_init (VisPluginData *plugin);
 int lv_stars_cleanup (VisPluginData *plugin);
@@ -79,181 +85,184 @@ VISUAL_PLUGIN_API_VERSION_VALIDATOR
 
 const VisPluginInfo *get_plugin_info (int *count)
 {
-	static const VisActorPlugin actor[] = {{
-		.requisition = lv_stars_requisition,
-		.palette = lv_stars_palette,
-		.render = lv_stars_render,
-		.vidoptions.depth =
-			VISUAL_VIDEO_DEPTH_8BIT |
-			VISUAL_VIDEO_DEPTH_32BIT
+    static const VisActorPlugin actor[] = {{
+        .requisition = lv_stars_requisition,
+        .palette = lv_stars_palette,
+        .render = lv_stars_render,
+        .vidoptions.depth =
+            VISUAL_VIDEO_DEPTH_8BIT |
+            VISUAL_VIDEO_DEPTH_32BIT
 
-	}};
+    }};
 
-	static const VisPluginInfo info[] = {{
-		.type = VISUAL_PLUGIN_TYPE_ACTOR,
+    static const VisPluginInfo info[] = {{
+        .type = VISUAL_PLUGIN_TYPE_ACTOR,
 
-		.plugname = "avs_stars",
-		.name = "Libvisual AVS Render: Starfield element",
-		.author = "Dennis Smit <ds@nerds-incorporated.org>",
-		.version = "0.1",
-		.about = "The Libvisual AVS Render: Starfield element",
-		.help = "This is the Starfield scope element for the libvisual AVS system",
+        .plugname = "avs_stars",
+        .name = "Libvisual AVS Render: Starfield element",
+        .author = "Dennis Smit <ds@nerds-incorporated.org>",
+        .version = "0.1",
+        .about = "The Libvisual AVS Render: Starfield element",
+        .help = "This is the Starfield scope element for the libvisual AVS system",
 
-		.init = lv_stars_init,
-		.cleanup = lv_stars_cleanup,
-		.events = lv_stars_events,
+        .init = lv_stars_init,
+        .cleanup = lv_stars_cleanup,
+        .events = lv_stars_events,
 
-		.plugin = VISUAL_OBJECT (&actor[0])
-	}};
+        .plugin = VISUAL_OBJECT (&actor[0])
+    }};
 
-	*count = sizeof (info) / sizeof (*info);
+    *count = sizeof (info) / sizeof (*info);
 
-	return info;
+    return info;
 }
 
 int lv_stars_init (VisPluginData *plugin)
 {
-	StarsPrivate *priv;
-	VisParamContainer *paramcontainer = visual_plugin_get_params (plugin);
-	int i;
+    StarsPrivate *priv;
+    VisParamContainer *paramcontainer = visual_plugin_get_params (plugin);
+    int i;
 
-	static VisParamEntryProxy params[] = {
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("enabled", 1, VISUAL_PARAM_LIMIT_NONE, "Enable Starfield"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("blend", 2, VISUAL_PARAM_LIMIT_NONE, "Blend"),
-		VISUAL_PARAM_LIST_ENTRY ("palette", "Colors"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("blendavg", 0x10, VISUAL_PARAM_LIMIT_NONE, "Blend average"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("warpSpeed", 0, VISUAL_PARAM_LIMIT_NONE, "Warp speed"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("MaxStars_set", 0, VISUAL_PARAM_LIMIT_NONE, "Number of stars"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("onbeat", 0, VISUAL_PARAM_LIMIT_NONE, "On-beat speed changes"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("spdBeat", 0, VISUAL_PARAM_LIMIT_NONE, "Beat speed"),
-		VISUAL_PARAM_LIST_ENTRY_INTEGER ("durFrames", 0, VISUAL_PARAM_LIMIT_NONE, "Frame duration"),
-		VISUAL_PARAM_LIST_END
-	};
+    static VisParamEntry params[] = {
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("enabled", 1),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("blend", 0),
+        VISUAL_PARAM_LIST_ENTRY ("palette"),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("blendavg", 0),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("warpSpeed", 0),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("MaxStars_set", 350),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("onbeat", 0),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("spdBeat", 4),
+        VISUAL_PARAM_LIST_ENTRY_INTEGER ("durFrames", 15),
+        VISUAL_PARAM_LIST_END
+    };
 
     
-	priv = visual_mem_new0 (StarsPrivate, 1);
-    priv->proxy = visual_object_get_private(VISUAL_OBJECT(plugin));
+    priv = visual_mem_new0 (StarsPrivate, 1);
+    priv->pipeline = visual_object_get_private(VISUAL_OBJECT(plugin));
 
-    if(priv->proxy == NULL) {
+    if(priv->pipeline == NULL) {
         visual_log(VISUAL_LOG_CRITICAL, "This element is part of the AVS plugin");
-        return 0;
+        return VISUAL_ERROR_GENERAL;
     }
-    visual_object_ref(VISUAL_OBJECT(priv->proxy));
-	visual_object_set_private (VISUAL_OBJECT (plugin), priv);
+    visual_object_ref(VISUAL_OBJECT(priv->pipeline));
+    visual_object_set_private (VISUAL_OBJECT (plugin), priv);
 
-	visual_palette_allocate_colors (&priv->pal, 1);
+    visual_palette_allocate_colors (&priv->pal, 1);
 
-	for (i = 0; i < priv->pal.ncolors; i++) {
-		priv->pal.colors[i].r = 0xff;
-		priv->pal.colors[i].g = 0xff;
-		priv->pal.colors[i].b = 0xff;
-	}
+    for (i = 0; i < priv->pal.ncolors; i++) {
+        priv->pal.colors[i].r = 0xff;
+        priv->pal.colors[i].g = 0xff;
+        priv->pal.colors[i].b = 0xff;
+    }
 
-	visual_param_container_add_many_proxy (paramcontainer, params);
+    priv->MaxStars = 350;
 
-	visual_param_entry_set_palette (visual_param_container_get (paramcontainer, "palette"), &priv->pal);
+    visual_param_container_add_many (paramcontainer, params);
 
-	visual_palette_free_colors (&priv->pal);
+    visual_param_entry_set_palette (visual_param_container_get (paramcontainer, "palette"), &priv->pal);
 
-	return 0;
+    visual_palette_free_colors (&priv->pal);
+
+    return 0;
 }
 
 int lv_stars_cleanup (VisPluginData *plugin)
 {
-	StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+    StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
 
-	visual_mem_free (priv);
+    visual_mem_free (priv);
 
-	return 0;
+    return 0;
 }
 
 int lv_stars_requisition (VisPluginData *plugin, int *width, int *height)
 {
-	return 0;
+    return 0;
 }
 
 int lv_stars_dimension (VisPluginData *plugin, VisVideo *video, int width, int height)
 {
-	visual_video_set_dimension (video, width, height);
+    visual_video_set_dimension (video, width, height);
 
-	return 0;
+    return 0;
 }
 
 int lv_stars_events (VisPluginData *plugin, VisEventQueue *events)
 {
-	StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
-	VisParamEntry *param;
-	VisEvent ev;
+    StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+    VisParamEntry *param;
+    VisEvent ev;
 
-	while (visual_event_queue_poll (events, &ev)) {
-		switch (ev.type) {
-			case VISUAL_EVENT_RESIZE:
-				lv_stars_dimension (plugin, ev.event.resize.video,
-						ev.event.resize.width, ev.event.resize.height);
-				break;
+    while (visual_event_queue_poll (events, &ev)) {
+        switch (ev.type) {
+            case VISUAL_EVENT_RESIZE:
+                lv_stars_dimension (plugin, ev.event.resize.video,
+                        ev.event.resize.width, ev.event.resize.height);
+                break;
 
-			case VISUAL_EVENT_PARAM:
-				param = ev.event.param.param;
+            case VISUAL_EVENT_PARAM:
+                param = ev.event.param.param;
 
-				if (visual_param_entry_is (param, "enabled"))
-					priv->enabled = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "blend"))
-					priv->blend = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "blendavg"))
-					priv->blendavg = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "warpSpeed"))
-					priv->warpSpeed = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "MaxStars_set"))
-					priv->MaxStars_set = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "onbeat"))
-					priv->onbeat = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "spdBeat"))
-					priv->spdBeat = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "durFrames"))
-					priv->durFrames = visual_param_entry_get_integer (param);
-				else if (visual_param_entry_is (param, "palette")) {
-					VisPalette *pal;
+                if (visual_param_entry_is (param, "enabled"))
+                    priv->enabled = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "blend"))
+                    priv->blend = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "blendavg"))
+                    priv->blendavg = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "warpSpeed"))
+                    priv->warpSpeed = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "MaxStars_set"))
+                    priv->MaxStars_set = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "onbeat"))
+                    priv->onbeat = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "spdBeat"))
+                    priv->spdBeat = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "durFrames"))
+                    priv->durFrames = visual_param_entry_get_integer (param);
+                else if (visual_param_entry_is (param, "palette")) {
+                    VisPalette *pal;
 
-					pal = visual_param_entry_get_palette (param);
+                    pal = visual_param_entry_get_palette (param);
 
-					visual_palette_free_colors (&priv->pal);
-					visual_palette_allocate_colors (&priv->pal, pal->ncolors);
-					visual_palette_copy (&priv->pal, pal);
+                    visual_palette_free_colors (&priv->pal);
+                    visual_palette_allocate_colors (&priv->pal, pal->ncolors);
+                    visual_palette_copy (&priv->pal, pal);
 
-					if (priv->cycler != NULL)
-						visual_object_unref (VISUAL_OBJECT (priv->cycler));
+                    if (priv->cycler != NULL)
+                        visual_object_unref (VISUAL_OBJECT (priv->cycler));
 
-					priv->cycler = avs_gfx_color_cycler_new (&priv->pal);
-					avs_gfx_color_cycler_set_mode (priv->cycler, AVS_GFX_COLOR_CYCLER_TYPE_TIME);
-					avs_gfx_color_cycler_set_time (priv->cycler, avs_config_standard_color_cycler_time ());
-				}
+                    priv->cycler = avs_gfx_color_cycler_new (&priv->pal);
+                    avs_gfx_color_cycler_set_mode (priv->cycler, AVS_GFX_COLOR_CYCLER_TYPE_TIME);
+                    avs_gfx_color_cycler_set_time (priv->cycler, avs_config_standard_color_cycler_time ());
+                }
 
-				break;
+                break;
 
-			default:
-				break;
-		}
-	}
+            default:
+                break;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 VisPalette *lv_stars_palette (VisPluginData *plugin)
 {
-	StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+    StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
 
-	return NULL;
+    return NULL;
 }
 
+#define max(a, b) (a>b?a:b)
 int lv_stars_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
 {
-	StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
-    AvsGlobalProxy *proxy = priv->proxy;
-    uint8_t *framebuffer = visual_video_get_pixels(video);
-	int w = video->width;
-	int h = video->height;
-    uint32_t color = visual_color_to_uint32(priv->pal->colors[0]);
-    int isBeat = proxy->isBeat;
+    StarsPrivate *priv = visual_object_get_private (VISUAL_OBJECT (plugin));
+    LVAVSPipeline *pipeline = priv->pipeline;
+    uint32_t *framebuffer = visual_video_get_pixels(video);
+    int w = video->width;
+    int h = video->height;
+    uint32_t color = 0xffffffff;//visual_color_to_uint32(&priv->pal.colors[0]);
+    int isBeat = pipeline->isBeat;
 
     int i=w*h;
     int NX,NY;
@@ -264,8 +273,8 @@ int lv_stars_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
     if (priv->onbeat && isBeat)
     {
         priv->CurrentSpeed = priv->spdBeat;
-        priv->incBeat = (priv->WarpSpeed - priv->CurrentSpeed) / (float)priv->durFrames;
-        nc = durFrames;
+        priv->incBeat = (priv->warpSpeed - priv->CurrentSpeed) / (float)priv->durFrames;
+        priv->nc = priv->durFrames;
     }
 
     if (priv->Width != w || priv->Height != h)
@@ -300,37 +309,49 @@ int lv_stars_render (VisPluginData *plugin, VisVideo *video, VisAudio *audio)
             create_star(priv, i);
     }
 
-    if (!nc)
-        priv->CurrentSpeed = priv->WarpSpeed;
+    if (!priv->nc)
+        priv->CurrentSpeed = priv->warpSpeed;
     else
     {
         priv->CurrentSpeed = max(0, priv->CurrentSpeed + priv->incBeat);
-        nc--;
+        priv->nc--;
     }
 
 
     return 0;
 }
 
-void initialize_stars(void)
+static int MulDiv(int nMultiplicand, int nMultiplier, int nDivisor) {
+    int ret;
+    if(nDivisor == 0) return -1;
+    if ( ( (nMultiplicand <  0) && (nMultiplier <  0) ) ||
+         ( (nMultiplicand >= 0) && (nMultiplier >= 0) ) )
+      ret = (((long long)nMultiplicand * nMultiplier) + (nDivisor/2)) / nDivisor;
+    else
+      ret = (((long long)nMultiplicand * nMultiplier) - (nDivisor/2)) / nDivisor;
+    return ret;
+}
+
+void initialize_stars(StarsPrivate *priv)
 {
   int i;
-  MaxStars = MulDiv(MaxStars_set,Width*Height,512*384);
-  if (MaxStars > 4095) MaxStars=4095;
-  for (i=0;i<MaxStars;i++)
+  srand(time(NULL));
+  priv->MaxStars = MulDiv(priv->MaxStars_set,priv->Width*priv->Height,512*384);
+  if (priv->MaxStars > 4095) priv->MaxStars=4095;
+  for (i=0;i<priv->MaxStars;i++)
     {
-    Stars[i].X=(rand()%Width)-Xoff;
-    Stars[i].Y=(rand()%Height)-Yoff;
-    Stars[i].Z=(float)(rand()%255);
-    Stars[i].Speed = (float)(rand()%9+1)/10;
+    priv->Stars[i].X=(rand()%priv->Width)-priv->Xoff;
+    priv->Stars[i].Y=(rand()%priv->Height)-priv->Yoff;
+    priv->Stars[i].Z=(float)(rand()%255);
+    priv->Stars[i].Speed = (float)(rand()%9+1)/10;
     }
 }
 
-void create_star(int A)
+void create_star(StarsPrivate *priv, int A)
 {
-  Stars[A].X = (rand()%Width)-Xoff;
-  Stars[A].Y = (rand()%Height)-Yoff;
-  Stars[A].Z = (float)Zoff;
+  priv->Stars[A].X = (rand()%priv->Width)-priv->Xoff;
+  priv->Stars[A].Y = (rand()%priv->Height)-priv->Yoff;
+  priv->Stars[A].Z = (float)priv->Zoff;
 }
 
 static unsigned int __inline BLEND_ADAPT(unsigned int a, unsigned int b, /*float*/int divisor)
